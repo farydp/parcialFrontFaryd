fb6016156cbbd432e695e1e9ff642121c696c9f0
Farydinho <farydalejandro1997@hotmail.com>
2024-08-14 17:16:55 -0500

    correccion posicion carpeta components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './assets/Components/Card'
+import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="App">
-        <h1>Carga de estyduabtes</h1>
+        <h1>Carga de estudiantes</h1>
         <form></form>
         <Card/>
       </div>
